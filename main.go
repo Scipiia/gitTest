28f7309e487d94ac7c8faa02485c35d6ae1c86cd
Scipiia <kuznetcov.artem.va@gmail.com>
2023-01-18 13:38:12 +0800

    two

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	fmt.Println("Hello git")
+	fmt.Println("buy buy git")
+	for i := 0; i < 12; i++ {
+		fmt.Println(i)
+		time.Sleep(1 * time.Second)
+	}
 }
